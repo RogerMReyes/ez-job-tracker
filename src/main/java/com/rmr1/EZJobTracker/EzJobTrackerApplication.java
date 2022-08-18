cb9e22fa981ca48ae0ba5528dc5e78ca14a917a8
RogerMReyes <Rogerrmr14@gmail.com>
2022-08-18 14:16:33 -0700

    Adding Base Spring Files

---
+++ src/main/java/com/rmr1/EZJobTracker/EzJobTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.rmr1.EZJobTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzJobTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzJobTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmr1/EZJobTracker/EzJobTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmr1.EZJobTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzJobTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
